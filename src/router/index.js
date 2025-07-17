2ebd001130ffcaea9caeaf3dfa0fd91410b7d361
guddnboy <hung3018@naver.com>
2025-07-17 16:11:27 +0900

    feat: 로그인 페이지 컴포넌트 추가

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import LoginView from '@/views/login/LoginView.vue';
 import MatchingView from '@/views/matching/MatchingView.vue';
 import MyPageView from '@/views/mypage/MyPageView.vue';
 import RankingView from '@/views/ranking/RankingView.vue';
-import TestView from '@/views/TestView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +11,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: TestView,
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
     {
       path: '/login',
