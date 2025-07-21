9494b62ac0b282ca2690f8e7138619a1c44088bd
yurissssss <yur1n9@kakao.com>
2025-07-21 21:44:11 +0900

    chore: 파일 위치 변경

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import MainPage from '@/pages/MainPage.vue';
+import HomePage from '@/pages/HomePage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'main',
-      component: MainPage,
+      name: 'home',
+      component: HomePage,
     },
   ],
 });
