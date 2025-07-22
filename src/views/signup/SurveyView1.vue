363c0cf16ab74d0532ab1522e2163045aad1d6b7
Gayeon07 <lgy3131@naver.com>
2025-07-22 21:43:35 +0900

    feat : 현재 유형 설문조사 페이지에 답변체크, 모달창 기능 추가

--- src/views/signup/SurveyView1.vue
+++ src/views/signup/SurveyView1.vue
@@ -10,7 +10,7 @@
       </div>
 
       <!-- 설문조사 문항 -->
-      <div class="flex flex-col gap-7">
+      <div class="flex flex-col gap-5">
         <div
           v-for="question in questions"
           :key="question.id"
@@ -36,56 +36,66 @@
             </div>
           </div>
 
-          <!-- 설문조사 응답 버튼과 라벨을 함께 -->
-          <div class="grid grid-cols-5 gap-0 px-4">
-            <template v-for="(option, index) in question.options" :key="index">
-              <!-- 각 옵션을 컨테이너로 묶기 -->
-              <div class="flex flex-col items-center">
-                <div
-                  @click="selectOption(question.id, option.value)"
-                  :class="[
-                    'w-6 h-6 rounded-full border-2 text-gray-300 border-limegreen-500 flex items-center justify-center text-xs cursor-pointer transition-colors',
-                    answers[question.id] === option.value
-                      ? 'bg-limegreen-100'
-                      : 'bg-white hover:bg-limegreen-100',
-                  ]"
-                >
-                  {{ index + 1 }}
-                </div>
-                <!-- 라벨 -->
-                <div
-                  @click="selectOption(question.id, option.value)"
-                  class="text-xs text-center text-gray-300 mt-1 whitespace-pre-line"
-                >
-                  {{ option.label }}
+          <div class="relative">
+            <div
+              class="absolute top-3 left-6 right-6 h-0.5 bg-limegreen-500 z-0"
+            ></div>
+
+            <!-- 설문조사 응답 버튼과 라벨 -->
+            <div class="grid grid-cols-5 gap-0 relative z-10">
+              <div v-for="(option, index) in question.options" :key="index">
+                <!-- 각 옵션을 컨테이너로 묶기 -->
+                <div class="flex flex-col items-center">
+                  <div
+                    @click="selectOption(question.id, option.value)"
+                    :class="[
+                      'w-6 h-6 rounded-full border-2 text-gray-300 border-limegreen-500 flex items-center justify-center text-xs cursor-pointer ',
+                      answers[question.id] === option.value
+                        ? 'bg-limegreen-100'
+                        : 'bg-white hover:bg-limegreen-100',
+                    ]"
+                  >
+                    {{ index + 1 }}
+                  </div>
+                  <!-- 라벨 -->
+                  <div
+                    class="text-xs text-center text-gray-300 mt-1 whitespace-pre-line"
+                  >
+                    {{ option.label }}
+                  </div>
                 </div>
               </div>
-
-              <!--  연결선 (마지막 항목 제외)
-              <div
-                v-if="index < question.options.length - 1"
-                class="flex-1 h-0.5 bg-limegreen-500 mt-3.5"
-              ></div> -->
-            </template>
+            </div>
           </div>
         </div>
       </div>
 
       <!-- 다음 버튼 -->
-      <div>
-        <button
-          @click="handleNext"
-          class="w-full bg-limegreen-500 text-white text-lg py-4 rounded-lg"
-        >
-          다음
-        </button>
-      </div>
+      <button
+        @click="handleNext"
+        class="w-full bg-limegreen-500 text-white text-lg py-4 rounded-lg"
+      >
+        다음
+      </button>
+
+      <!-- 모달 -->
+      <LoginModal
+        v-if="showModal"
+        title="알림"
+        message="모든 항목에 응답해주세요."
+        buttonText="확인"
+        @close="showModal = false"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
 import { reactive, ref } from 'vue';
+
+import router from '@/router';
+
+import LoginModal from '../login/components/LoginModal.vue';
 
 // 답변
 const answers = reactive({
@@ -97,6 +107,24 @@ const answers = reactive({
 
 // 모달 상태
 const showModal = ref(false);
+
+// 응답 선택
+function selectOption(questionId, value) {
+  answers[questionId] = value;
+}
+
+// 다음 버튼 클릭 시
+function handleNext() {
+  //answers의 value들을 배열에 담아 값이 null이 아닌지(모든 항목이 선택되었는지) 확인
+  const isAllAnswered = Object.values(answers).every(value => value !== null);
+  if (!isAllAnswered) {
+    showModal.value = true;
+    return;
+  }
+
+  console.log('답변:', answers);
+  router.push({ name: 'survey2' });
+}
 
 // 질문 데이터
 const questions = [
