6c88485a6d63348ed4bab96155fc4274c5badb3c
Gayeon07 <lgy3131@naver.com>
2025-07-29 13:16:09 +0900

    feat : 추구미 변경 가능 기간 알림창 구현

--- src/views/mypage/MyPageView.vue
+++ src/views/mypage/MyPageView.vue
@@ -16,16 +16,27 @@
             class="w-[120px] mt-7"
           />
           <!-- 추구미 유형명 -->
-          <button
-            class="flex justify-center items-center gap-1 bg-green text-white px-2.5 py-2 rounded-full text-sm text-center tracking-widest"
-          >
-            <div class="flex items-center leading-none text-[14px]">
-              저축실천형
+          <div class="relative group">
+            <button
+              class="flex justify-center items-center gap-1 bg-green text-white px-2.5 py-1.5 rounded-full text-sm text-center tracking-widest"
+            >
+              <div class="flex items-center leading-none text-[14px]">
+                저축실천형
+              </div>
+              <div class="flex justify-center items-center">
+                <img :src="edit" alt="수정 아이콘" class="h-3 w-3" />
+              </div>
+            </button>
+
+            <!-- 안내창 -->
+            <div
+              v-if="!isEditableDay"
+              class="absolute w-30 left-full top-1/2 -translate-y-1/2 ml-2 whitespace-pre-wrap bg-ivory border-3 border-limegreen-500 text-limegreen-700 text-xs rounded-[10px] px-2 py-1.5 z-10 opacity-0 group-hover:opacity-100 transition-opacity shadow-[2px_2px_6px_0px] shadow-limegreen-500"
+            >
+              변경 가능 시간<br />
+              매달 첫째 주 일요일
             </div>
-            <div class="flex justify-center items-center">
-              <img :src="edit" alt="수정 아이콘" class="h-3 w-3" />
-            </div>
-          </button>
+          </div>
         </div>
 
         <!-- 닉네임 -->
@@ -84,7 +95,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 import character_savings from '@/assets/img/characters/character_savings.png';
 import edit from '@/assets/img/icons/feature/icon_edit.png';
@@ -96,6 +107,28 @@ import router from '@/router';
 import MyPageBtn from './components/MyPageBtn.vue';
 
 const showModal = ref(false);
+
+const isEditableDay = computed(() => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+
+  // 해당 달의 1일부터 7일까지
+  for (let day = 1; day <= 7; day++) {
+    const date = new Date(year, month, day);
+
+    // 일요일 찾음
+    if (date.getDay() === 0) {
+      //오늘이 첫째주 일요일이라면
+      return (
+        today.getDate() === date.getDate() &&
+        today.getMonth() === date.getMonth() &&
+        today.getFullYear() === date.getFullYear()
+      );
+    }
+  }
+  return false;
+});
 
 const logout = () => {
   localStorage.clear();
