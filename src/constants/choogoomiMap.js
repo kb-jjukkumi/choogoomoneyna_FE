28acf319301306278b4fe00a13974d371ce7f29d
yurissssss <yur1n9@kakao.com>
2025-07-30 10:52:30 +0900

    feat: 추구미유형 알파벳 - 이름 - 캐릭터이미지  매핑

---
+++ src/constants/choogoomiMap.js
@@ -0,0 +1,35 @@
+export const CHOOGOOMI_MAP = {
+  A: {
+    0: {
+      choogoomiType: '지출제로형 ',
+      character: '../../assets/img/characters/character_zero_1.png',
+    },
+    1: { choogoomiType: '지출제로형', character: '이미지 경로' },
+    2: { choogoomiType: '지출제로형', character: '이미지 경로' },
+    3: { choogoomiType: '지출제로형', character: '이미지 경로' },
+  },
+  B: {
+    0: { choogoomiType: '합리소비형', character: '이미지 경로' },
+    1: { choogoomiType: '합리소비형', character: '이미지 경로' },
+    2: { choogoomiType: '합리소비형', character: '이미지 경로' },
+    3: { choogoomiType: '합리소비형', character: '이미지 경로' },
+  },
+  C: {
+    0: { choogoomiType: '저축실천형', character: '이미지 경로' },
+    1: { choogoomiType: '저축실천형', character: '이미지 경로' },
+    2: { choogoomiType: '저축실천형', character: '이미지 경로' },
+    3: { choogoomiType: '저축실천형', character: '이미지 경로' },
+  },
+  D: {
+    0: { choogoomiType: '투자도전형', character: '이미지 경로' },
+    1: { choogoomiType: '투자도전형', character: '이미지 경로' },
+    2: { choogoomiType: '투자도전형', character: '이미지 경로' },
+    3: { choogoomiType: '투자도전형', character: '이미지 경로' },
+  },
+  E: {
+    0: { choogoomiType: '금융탐구형', character: '이미지 경로' },
+    1: { choogoomiType: '금융탐구형', character: '이미지 경로' },
+    2: { choogoomiType: '금융탐구형', character: '이미지 경로' },
+    3: { choogoomiType: '금융탐구형', character: '이미지 경로' },
+  },
+};
