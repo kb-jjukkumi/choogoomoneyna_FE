5ed2245e13a67db4e820dd79bafccfe8aef94ccb
yurissssss <yur1n9@kakao.com>
2025-08-04 18:59:48 +0900

    feat: 매칭 정보 api 추가

---
+++ src/api/matchingApi.js
@@ -0,0 +1,13 @@
+// axios 라이브러리 불러오기 -> 커스텀 인스턴스 만들기
+import axiosInstance from './axios';
+
+// 매칭 메인 정보
+export const fetchMatchingMain = async () => {
+  try {
+    const response = await axiosInstance.get('/api/matching/main');
+    return response.data;
+  } catch (error) {
+    console.error('매칭 메인 정보 불러오기 실패:', error);
+    throw error;
+  }
+};
