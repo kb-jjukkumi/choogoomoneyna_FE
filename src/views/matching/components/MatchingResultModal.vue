d774f4224e78b61ed14163b395745764f90f334c
Gayeon07 <lgy3131@naver.com>
2025-08-05 10:20:25 +0900

    feat : 나와 상대방 닉네임&점수를 가져옴

--- src/views/matching/components/MatchingResultModal.vue
+++ src/views/matching/components/MatchingResultModal.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="fixed inset-0 flex items-center justify-center bg-black/70 z-20">
+  <div
+    v-if="isLoaded"
+    class="fixed inset-0 flex items-center justify-center bg-black/70 z-20"
+  >
     <div
       class="flex flex-col gap-4 bg-limegreen-500 rounded-4xl p-5 items-center"
     >
@@ -9,13 +12,19 @@
         <div class="bg-ivory flex rounded-2xl p-5">
           <div class="flex gap-6 text-sm text-center items-center">
             <div>
-              <div class="text-limegreen-900">카카오대학교라이언</div>
-              <div class="text-green">50점</div>
+              <div class="text-limegreen-900">
+                {{ matchingResult.myMissionProgressList[0].userNickname }}
+              </div>
+              <div class="text-green">{{ matchingResult.myTotalScore }}점</div>
             </div>
             <div class="text-limegreen-900 text-lg">VS</div>
             <div>
-              <div class="text-limegreen-900">카카오대학교어피치</div>
-              <div class="text-green">20점</div>
+              <div class="text-limegreen-900">
+                {{ matchingResult.opponentMissionProgressList[0].userNickname }}
+              </div>
+              <div class="text-green">
+                {{ matchingResult.opponentTotalScore }}점
+              </div>
             </div>
           </div>
         </div>
@@ -39,7 +48,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 
 import axiosInstance from '@/api/axios';
 
@@ -50,13 +59,23 @@ const props = defineProps({
   title: { type: String, required: true },
 });
 
+const matchingResult = reactive({
+  myMissionProgressList: [],
+  opponentMissionProgressList: [],
+  myTotalScore: 0,
+  opponentTotalScore: 0,
+});
+
+const isLoaded = ref(false);
+
 onMounted(async () => {
   try {
     const { data } = await axiosInstance.get(
       `/api/matching/history?roundNumber=${props.roundNumber}`
     );
-    console.log(data);
-  } catch {
+    Object.assign(matchingResult, data);
+    isLoaded.value = true;
+  } catch (error) {
     console.log(error);
   }
 });
