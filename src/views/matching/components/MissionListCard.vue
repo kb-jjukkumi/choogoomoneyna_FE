a08868d02067cc46b48d6f44cd788df1e784051b
Gayeon07 <lgy3131@naver.com>
2025-08-08 11:18:55 +0900

    chore : 텍스트 색상 미지정 수정

--- src/views/matching/components/MissionListCard.vue
+++ src/views/matching/components/MissionListCard.vue
@@ -24,7 +24,7 @@
             :class="[
               'text-sm',
               'mr-5',
-              isMissionCompleted(mission) ? 'text-' : 'text-gray-300',
+              isMissionCompleted(mission) ? 'text-green' : 'text-gray-300',
             ]"
           >
             {{ mission.missionTitle }}
