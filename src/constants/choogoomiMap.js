4a98d823064c1a4bd428c9a49a0d30eecf5f62e8
guddnboy <hung3018@naver.com>
2025-08-11 13:06:14 +0900

    refactor: 추구미 타입, 레벨 데이터 구조 변경

--- src/constants/choogoomiMap.js
+++ src/constants/choogoomiMap.js
@@ -1,127 +1,52 @@
 export const CHOOGOOMI_MAP = [
   {
     choogoomiName: 'A',
+    choogoomiType: '지출제로형',
     userLevel: {
-      0: {
-        choogoomiType: '지출제로형',
-        character: '../../assets/img/characters/A/0.png',
-        profile: '../../assets/img/characters/A/0.png',
-      },
-      1: {
-        choogoomiType: '지출제로형',
-        character: '../../assets/img/characters/A/1.png',
-        profile: '../../assets/img/characters/A/1.png',
-      },
-      2: {
-        choogoomiType: '지출제로형',
-        character: '../../assets/img/characters/A/2.png',
-        profile: '../../assets/img/characters/A/2.png',
-      },
-      3: {
-        choogoomiType: '지출제로형',
-        character: '../../assets/img/characters/A/3.png',
-        profile: '../../assets/img/characters/A/3.png',
-      },
+      0: { image_url: '../../assets/img/characters/A/0.png' },
+      1: { image_url: '../../assets/img/characters/A/1.png' },
+      2: { image_url: '../../assets/img/characters/A/2.png' },
+      3: { image_url: '../../assets/img/characters/A/3.png' },
     },
   },
   {
     choogoomiName: 'B',
+    choogoomiType: '합리소비형',
     userLevel: {
-      0: {
-        choogoomiType: '합리소비형',
-        character: '../../assets/img/characters/B/0.png',
-        profile: '../../assets/img/characters/B/0.png',
-      },
-      1: {
-        choogoomiType: '합리소비형',
-        character: '../../assets/img/characters/B/1.png',
-        profile: '../../assets/img/characters/B/1.png',
-      },
-      2: {
-        choogoomiType: '합리소비형',
-        character: '../../assets/img/characters/B/2.png',
-        profile: '../../assets/img/characters/B/2.png',
-      },
-      3: {
-        choogoomiType: '합리소비형',
-        character: '../../assets/img/characters/B/3.png',
-        profile: '../../assets/img/characters/B/3.png',
-      },
+      0: { image_url: '../../assets/img/characters/B/0.png' },
+      1: { image_url: '../../assets/img/characters/B/1.png' },
+      2: { image_url: '../../assets/img/characters/B/2.png' },
+      3: { image_url: '../../assets/img/characters/B/3.png' },
     },
   },
   {
     choogoomiName: 'C',
+    choogoomiType: '저축실천형',
     userLevel: {
-      0: {
-        choogoomiType: '저축실천형',
-        character: '../../assets/img/characters/C/0.png',
-        profile: '../../assets/img/characters/C/0.png',
-      },
-      1: {
-        choogoomiType: '저축실천형',
-        character: '../../assets/img/charactersC/_1.png',
-        profile: '../../assets/img/characters/C/1.png',
-      },
-      2: {
-        choogoomiType: '저축실천형',
-        character: '../../assets/img/characters/C/2.png',
-        profile: '../../assets/img/characters/C/2.png',
-      },
-      3: {
-        choogoomiType: '저축실천형',
-        character: '../../assets/img/characters/C/3.png',
-        profile: '../../assets/img/characters/C/3.png',
-      },
+      0: { image_url: '../../assets/img/characters/C/0.png' },
+      1: { image_url: '../../assets/img/characters/C/1.png' },
+      2: { image_url: '../../assets/img/characters/C/2.png' },
+      3: { image_url: '../../assets/img/characters/C/3.png' },
     },
   },
   {
     choogoomiName: 'D',
+    choogoomiType: '투자도전형',
     userLevel: {
-      0: {
-        choogoomiType: '투자도전형',
-        character: '../../assets/img/characters/D/0.png',
-        profile: '../../assets/img/characters/D/0.png',
-      },
-      1: {
-        choogoomiType: '투자도전형',
-        character: '../../assets/img/characters/D/1.png',
-        profile: '../../assets/img/characters/D/1.png',
-      },
-      2: {
-        choogoomiType: '투자도전형',
-        character: '../../assets/img/characters/D/2.png',
-        profile: '../../assets/img/characters/D/2.png',
-      },
-      3: {
-        choogoomiType: '투자도전형',
-        character: '../../assets/img/characters/D/3.png',
-        profile: '../../assets/img/characters/D/3.png',
-      },
+      0: { image_url: '../../assets/img/characters/D/0.png' },
+      1: { image_url: '../../assets/img/characters/D/1.png' },
+      2: { image_url: '../../assets/img/characters/D/2.png' },
+      3: { image_url: '../../assets/img/characters/D/3.png' },
     },
   },
   {
     choogoomiName: 'E',
+    choogoomiType: '금융탐구형',
     userLevel: {
-      0: {
-        choogoomiType: '금융탐구형',
-        character: '../../assets/img/characters/E/0.png',
-        profile: '../../assets/img/characters/E/0.png',
-      },
-      1: {
-        choogoomiType: '금융탐구형',
-        character: '../../assets/img/characters/E/1.png',
-        profile: '../../assets/img/characters/E/1.png',
-      },
-      2: {
-        choogoomiType: '금융탐구형',
-        character: '../../assets/img/characters/E/2.png',
-        profile: '../../assets/img/characters/E/2.png',
-      },
-      3: {
-        choogoomiType: '금융탐구형',
-        character: '../../assets/img/characters/E/3.png',
-        profile: '../../assets/img/characters/E/3.png',
-      },
+      0: { image_url: '../../assets/img/characters/E/0.png' },
+      1: { image_url: '../../assets/img/characters/E/1.png' },
+      2: { image_url: '../../assets/img/characters/E/2.png' },
+      3: { image_url: '../../assets/img/characters/E/3.png' },
     },
   },
 ];
