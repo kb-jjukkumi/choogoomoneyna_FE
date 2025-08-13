4282204805929c04b7d030ddf325a25d084247e7
guddnboy <hung3018@naver.com>
2025-08-13 14:23:22 +0900

    feat: 카카오 로그인을 위한 중간 페이지 컴포넌트 추가

---
+++ src/views/kakao/KakaoLoginView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <LoadingScreen />
+  </div>
+</template>
+<script setup>
+import LoadingScreen from '@/components/LoadingScreen.vue';
+</script>
