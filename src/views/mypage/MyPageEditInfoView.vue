4a3bf77b3e9f38c7fee5f5fa92e7fa18b6af7c79
guddnboy <hung3018@naver.com>
2025-08-13 13:41:24 +0900

    fix: 회원 정보 수정 레이아웃 수정

--- src/views/mypage/MyPageEditInfoView.vue
+++ src/views/mypage/MyPageEditInfoView.vue
@@ -1,127 +1,138 @@
 <template>
-  <div class="relative flex justify-center w-full">
+  <div class="relative flex justify-center w-full flex-col">
     <TopNavigation :show-back="true" :show-logo-text="false" />
+    <div class="flex font-bold text-2xl justify-center h-14 items-center mt-16">
+      회원 정보 수정
+    </div>
     <div class="bg-ivory flex flex-col gap-7 w-full px-6">
       <!-- 타이틀 -->
-      <div class="flex font-bold text-xl justify-center h-14 items-center">
-        회원 정보 수정
-      </div>
 
       <!-- 입력 폼 -->
-      <form class="flex flex-col gap-5" @submit.prevent="handleUpdate">
-        <!--닉네임-->
-        <div class="flex flex-col gap-2">
-          <div class="flex flex-col">
-            <label for="nickname" class="block mb-1 font-bold">닉네임</label>
-            <div class="flex gap-3">
-              <input
-                v-model="newNickname"
-                id="nickname"
-                type="text"
-                placeholder="한글,영문,숫자 2~10자리"
-                class="border-2 border-limegreen-500 flex-2 w-full h-11 rounded-[10px] bg-white px-3 py-3"
-                @input="onNicknameInput"
-              />
-              <button
-                class="flex-1 w-full h-11 text-white bg-limegreen-500 rounded-[10px] disabled:opacity-50"
-                type="button"
-                @click="handleCheckName"
-                :disabled="isNameChecked || !isNicknameValid"
+      <form
+        class="flex flex-col justify-center mt-16"
+        @submit.prevent="handleUpdate"
+      >
+        <div class="flex flex-col h-full justify-center gap-10">
+          <div class="flex flex-col gap-2">
+            <!--닉네임-->
+            <div class="flex flex-col gap2">
+              <div class="flex flex-col">
+                <label for="nickname" class="block mb-1 font-bold"
+                  >닉네임</label
+                >
+                <div class="flex gap-3">
+                  <input
+                    v-model="newNickname"
+                    id="nickname"
+                    type="text"
+                    placeholder="한글,영문,숫자 2~10자리"
+                    class="border-2 border-limegreen-500 flex-2 w-full h-11 rounded-[10px] bg-white px-3 py-3"
+                    @input="onNicknameInput"
+                  />
+                  <button
+                    class="flex-1 w-full h-11 text-white bg-limegreen-500 rounded-[10px] disabled:opacity-50"
+                    type="button"
+                    @click="handleCheckName"
+                    :disabled="isNameChecked || !isNicknameValid"
+                  >
+                    {{ isNameChecking ? '확인 중...' : '중복 확인' }}
+                  </button>
+                </div>
+              </div>
+              <p
+                class="h-3 text-xs"
+                :class="
+                  nameErrorMessage ? 'text-limegreen-700' : 'text-transparent'
+                "
               >
-                {{ isNameChecking ? '확인 중...' : '중복 확인' }}
-              </button>
+                {{ nameErrorMessage }}
+              </p>
+            </div>
+
+            <!--이메일-->
+            <div class="mb-5">
+              <label for="email" class="mb-1 block font-bold">이메일</label>
+              <input
+                v-model="userEmail"
+                id="email"
+                type="email"
+                class="border-2 border-limegreen-500 flex-2 w-full h-11 rounded-lg bg-limegreen-100 px-3 py-3 text-limegreen-700"
+                disabled
+              />
+            </div>
+            <!--현재 비밀번호-->
+            <div class="flex flex-col gap-2">
+              <div>
+                <label for="password" class="mb-1 block font-bold"
+                  >현재 비밀번호</label
+                >
+                <input
+                  v-model="currentPassword"
+                  id="password"
+                  type="password"
+                  placeholder="현재 비밀번호 입력"
+                  style="font-family: Arial, sans-serif"
+                  class="border-2 border-limegreen-500 flex-2 w-full h-11 rounded-[10px] bg-white px-3 py-3 placeholder:font-jua"
+                  @input="validateCurrentPassword"
+                />
+              </div>
+
+              <p
+                class="h-3 text-xs"
+                :class="
+                  CurrnetPwdErrorMessage ? 'text-red-500' : 'text-transparent'
+                "
+              >
+                {{ CurrnetPwdErrorMessage }}
+              </p>
+            </div>
+            <!--새 비밀번호-->
+            <div class="flex flex-col gap-2">
+              <div>
+                <label for="newPassword" class="mb-1 block font-bold"
+                  >새 비밀번호</label
+                >
+                <input
+                  v-model="newPassword"
+                  id="newPassword"
+                  type="password"
+                  placeholder="새 비밀번호를 입력해주세요."
+                  style="font-family: Arial, sans-serif"
+                  class="border-2 border-limegreen-500 flex-2 w-full h-11 rounded-[10px] bg-white px-3 py-3 placeholder:font-jua"
+                  @input="validateNewPassword"
+                />
+              </div>
+              <div>
+                <label for="newPassword2" class="my-1 block font-bold"
+                  >새 비밀번호 확인</label
+                >
+                <input
+                  v-model="newPassword2"
+                  id="newPassword2"
+                  type="password"
+                  placeholder="새 비밀번호를 한 번 더 입력해주세요"
+                  style="font-family: Arial, sans-serif"
+                  class="border-2 border-limegreen-500 flex-2 w-full h-11 rounded-[10px] bg-white px-3 py-3 placeholder:font-jua"
+                  @input="validateNewPassword"
+                />
+              </div>
+              <p
+                class="h-3 text-xs"
+                :class="
+                  NewPwdErrorMessage ? 'text-red-500' : 'text-transparent'
+                "
+              >
+                {{ NewPwdErrorMessage }}
+              </p>
             </div>
           </div>
-          <p
-            class="h-3 text-xs"
-            :class="
-              nameErrorMessage ? 'text-limegreen-700' : 'text-transparent'
-            "
+          <button
+            type="submit"
+            class="bg-limegreen-500 text-ivory mt-30! w-full rounded-[10px] py-3 text-lg!"
           >
-            {{ nameErrorMessage }}
-          </p>
+            수정 완료
+          </button>
         </div>
-
-        <!--이메일-->
-        <div class="mb-5">
-          <label for="email" class="mb-1 block font-bold">이메일</label>
-          <input
-            v-model="userEmail"
-            id="email"
-            type="email"
-            class="border-2 border-limegreen-500 flex-2 w-full h-11 rounded-lg bg-limegreen-100 px-3 py-3 text-limegreen-700"
-            disabled
-          />
-        </div>
-        <!--현재 비밀번호-->
-        <div class="flex flex-col gap-2">
-          <div>
-            <label for="password" class="mb-1 block font-bold"
-              >현재 비밀번호</label
-            >
-            <input
-              v-model="currentPassword"
-              id="password"
-              type="password"
-              placeholder="현재 비밀번호 입력"
-              style="font-family: Arial, sans-serif"
-              class="border-2 border-limegreen-500 flex-2 w-full h-11 rounded-[10px] bg-white px-3 py-3 placeholder:font-jua"
-              @input="validateCurrentPassword"
-            />
-          </div>
-
-          <p
-            class="h-3 text-xs"
-            :class="
-              CurrnetPwdErrorMessage ? 'text-red-500' : 'text-transparent'
-            "
-          >
-            {{ CurrnetPwdErrorMessage }}
-          </p>
-        </div>
-        <!--새 비밀번호-->
-        <div class="flex flex-col gap-2">
-          <div>
-            <label for="newPassword" class="mb-1 block font-bold"
-              >새 비밀번호</label
-            >
-            <input
-              v-model="newPassword"
-              id="newPassword"
-              type="password"
-              placeholder="새 비밀번호를 입력해주세요."
-              style="font-family: Arial, sans-serif"
-              class="border-2 border-limegreen-500 flex-2 w-full h-11 rounded-[10px] bg-white px-3 py-3 placeholder:font-jua"
-              @input="validateNewPassword"
-            />
-          </div>
-          <div>
-            <label for="newPassword2" class="my-1 block font-bold"
-              >새 비밀번호 확인</label
-            >
-            <input
-              v-model="newPassword2"
-              id="newPassword2"
-              type="password"
-              placeholder="새 비밀번호를 한 번 더 입력해주세요"
-              style="font-family: Arial, sans-serif"
-              class="border-2 border-limegreen-500 flex-2 w-full h-11 rounded-[10px] bg-white px-3 py-3 placeholder:font-jua"
-              @input="validateNewPassword"
-            />
-          </div>
-          <p
-            class="h-3 text-xs"
-            :class="NewPwdErrorMessage ? 'text-red-500' : 'text-transparent'"
-          >
-            {{ NewPwdErrorMessage }}
-          </p>
-        </div>
-        <button
-          type="submit"
-          class="bg-limegreen-500 text-white mt-2 w-full rounded-[10px] py-3 text-lg font-normal"
-        >
-          수정 완료
-        </button>
       </form>
     </div>
     <ConfirmModal
